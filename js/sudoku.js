75ecf8ef97f8a526b4974dd6df535ab4fced060b
FungYua <fungyua@gmail.com>
2021-11-02 13:13:02 +0800

    Init

---
+++ js/sudoku.js
@@ -0,0 +1,62 @@
+class Game extends Sudoku {
+  constructor(options) {
+    
+  }
+
+  start(el) {
+
+  }
+
+  reset() {
+
+  }
+
+  clean() {
+
+  }
+
+  back() {
+
+  }
+
+  changeOption() {
+
+  }
+
+  changeTheme() {
+
+  }
+
+  suspend() {
+
+  }
+
+  tip() {
+
+  }
+
+  erase() {
+
+  }
+}
+
+class Sudoku {
+
+}
+
+
+function create(el, ) {
+
+}
+
+function fillNumber() {}
+
+function computeArray() {}
+
+function createArena(row, col) {
+  for (let r = 0; r < row; r++) {
+    for (let c = 0; c < col; c++) {
+      app.append()
+    }
+  }
+}
